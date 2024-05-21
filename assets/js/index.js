f6c10585bdbca6993744673caf6589fcf0c00bc1
GonzalezNacho <nacho.ldm63@gmail.com>
2024-05-21 14:19:05 -0300

    agregando el index js en todos los htmls y los id del form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
 /*prueba*/
-document.getElementById('form-buscar').addEventListener('submit', function() {
-    document.getElementById('boton-buscar').value = '';
-})
+document.getElementById('form-buscar').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const busqueda = document.getElementById('input-buscar').value;
+    if (busqueda === '') {
+        alert('Por favor, introduce una palabra clave');
+    } else {
+        window.location.href = `./bilbioteca.html`
+        alert(`Buscando por: ${busqueda}`);
+        document.getElementById('input-buscar').value = '';
+        document.getElementById('form-buscar').reset();
+    }
+}) 

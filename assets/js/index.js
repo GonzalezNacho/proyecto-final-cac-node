6493fbeff86a9c02d09361394353319c38fbbd2b
GonzalezNacho <nacho.ldm63@gmail.com>
2024-05-24 22:19:31 -0300

    menu responsive terminado

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,11 @@ document.getElementById('form-buscar').addEventListener('submit', (e) => {
         window.location.href = `busqueda.html?search=${busqueda}`;
     }
 })
+
+const botonNav = document.getElementById('icono-nav');
+const menuResponsive = () => {
+    const nav = document.getElementById("nav");
+    nav.className==="" ? nav.className = "responsive" : nav.className = "";
+}
+
+botonNav.addEventListener('click', menuResponsive);

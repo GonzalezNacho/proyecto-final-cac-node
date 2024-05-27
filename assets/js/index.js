57ceeb4479146dbcee7dde426771644e9a79b66e
GonzalezNacho <nacho.ldm63@gmail.com>
2024-05-27 19:56:12 -0300

    cambiando la cant min de busqueda a 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 document.getElementById('form-buscar').addEventListener('submit', (e) => {
     e.preventDefault();
     const busqueda = document.getElementById('input-buscar').value;
-    if (busqueda.length <= 3) {
+    if (busqueda.length < 3) {
         alert(`Por favor, introduce una palabra clave de 3 o más caracteres ${busqueda} `);
         document.getElementById('input-buscar').value = '';
         document.getElementById('form-buscar').reset();

e423bcd0ed40b932926392f12bd01acb15759494
GonzalezNacho <nacho.ldm63@gmail.com>
2024-05-28 10:46:56 -0300

    agregando busqueda mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,16 @@
-document.getElementsByClassName('form-buscar').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const busqueda = document.getElementById('input-buscar').value;
-    if (busqueda.length < 3) {
-        alert(`Por favor, introduce una palabra clave de 3 o más caracteres ${busqueda} `);
-        document.getElementById('input-buscar').value = '';
-        document.getElementById('form-buscar').reset();
-    } else {
-        window.location.href = `busqueda.html?search=${busqueda}`;
-    }
-})
+const formsHtml = document.getElementsByClassName('form-buscar');
+for (const form of formsHtml) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const busqueda = form.children[0].value ;
+        if (busqueda.length < 3) {
+            alert(`Por favor, introduce una palabra clave de 3 o más caracteres ${busqueda} `);
+            form.children[0].value  = '';
+        } else {
+            window.location.href = `busqueda.html?search=${busqueda}`;
+        }
+    })
+}
 
 const botonNav = document.getElementById('icono-nav');
 const menuResponsive = () => {

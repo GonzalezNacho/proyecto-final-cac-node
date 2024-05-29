0c9e222c631d005f1663ed8aa3327e71ea71b0e2
GonzalezNacho <nacho.ldm63@gmail.com>
2024-05-29 19:32:30 -0300

    hecho lo de la verificacion de formulario

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,75 +1,29 @@
-let nombreCorrecto = false;
-let telefonoCorrecto = false;
-let emailCorrecto = false;
-let comentarioConContenido = false;
-let nombre = document.getElementById("nombre");
-let email = document.getElementById("correo");
-let comentario = document.getElementById("comentario");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("correo");
+const comentario = document.getElementById("comentario");
+const errores = document.getElementById("warnings");
+const form = document.getElementById("contact-form");
 
-
-function chequearDatosComentario() {
-    chequearMail(email.value);
-    console.log("el numero de telefono esta correcto: " + telefono.checkValidity())
-    nombreCorrecto = nombre.value.length > 0;
-    comentarioConContenido = comentario.value.length > 0;
-    if (emailCorrecto && nombreCorrecto && comentarioConContenido && telefonoCorrecto) {
-        mostrarModal("Confirmar envío", "¿Está seguro que quiere enviar el formulario?");
-        agregarDatosAlModal();
-
+form.addEventListener("submit", function (event) {
+    let warnings = "";
+    let entrar = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    event.preventDefault();
+    if (nombre.value.length < 6) {
+        warnings += `El nombre tiene que ser mayor a 6 caracteres <br>`;
+        entrar = true;
+    }
+    if (!regexEmail.test(email.value)) {
+        warnings += `El email no es valido <br>`;
+        entrar = true;
+    }
+    if (comentario.value.length < 20) {
+        warnings += `El comentario debe ser mayor a 20 caracteres <br>`;
+        entrar = true;  
+    }
+    if (entrar) {
+        errores.innerHTML = warnings;
     } else {
-        mostrarError();
+        form.submit();
     }
-}
-
-function mostrarError() {
-
-    if (nombreCorrecto == false) {
-        mostrarModal("Error al enviar comentario", "El campo Nombre y Apellido no puede estar vacio")
-
-    }
-    if (emailCorrecto == false) {
-        mostrarModal("Error al enviar comentario", "El email es inválido")
-
-    }
-    if (comentarioConContenido == false) {
-        mostrarModal("Error al enviar comentario", "El campo del comentario no puede estar vacio")
-
-    }
-
-}
-
-function chequearMail(string) {
-    let posicionDelPrimerArroba = string.indexOf("@");
-    let posicionDelUltimoArroba = string.lastIndexOf("@");
-    let posicionDelPunto = string.lastIndexOf(".");
-    if (posicionDelPrimerArroba == posicionDelUltimoArroba) {
-        if (posicionDelPunto > posicionDelPrimerArroba) {
-            emailCorrecto = true;
-        }
-    }
-}
-
-
-function mostrarModal(titulo, descripcion) {
-    let elementoTitulo = document.getElementById('staticBackdropLabel');
-    let elementoContenido = document.getElementById('textModal');
-
-    elementoTitulo.innerHTML = titulo;
-    elementoContenido.innerHTML = descripcion;
-
-
-    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
-        keyboard: false
-    });
-    myModal.show();
-}
-
-function agregarDatosAlModal() {
-    let listaDeDatos = '<ul>';
-    listaDeDatos += '<li>Nombre: ' + nombre.value + '</li>';
-    listaDeDatos += '<li>Email: ' + email.value + '</li>';
-    listaDeDatos += '<li>Teléfono:' + telefono.value + '</li>';
-    listaDeDatos += '<li>Comentario: ' + comentario.value + '</li>';
-    listaDeDatos += '</ol>';
-    document.getElementById('textModal').innerHTML = listaDeDatos;
-}
+})
